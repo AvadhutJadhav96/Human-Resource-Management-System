1a2c5e3314720d642824d660766a922420a4bfdc
AvadhutJadhav96 <avadhutjadhav966@gmail.com>
2025-03-16 19:34:49 +0530

    Create basic structure of project

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type MongoInstance struct {
+	Client
+	Db
+}
+
+var mg MongoInstance
+
+const dbName = "fiber-here"
+const mongoURI = "mongodb://localhost:27017"
+
+type Employee struct {
+	ID     string
+	Name   string
+	Salary float64
+	Age    float64
+}
+
+func Connect() error {
+
+}
+
+func main() {
+
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+	app := fiber.New() //just like starting an app in express
+
+	app.Get("/employee", func(c *fiber.Ctx) error {
+
+	})
+	//here c is for response and request
+
+	app.Post("/employee")
+	app.Put("/employee/:id")
+	app.Delete("/employee/:id")
+}
