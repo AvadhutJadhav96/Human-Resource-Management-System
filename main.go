7795f4c43b0e0dccaf014898bc25b9c2a7db8996
AvadhutJadhav96 <avadhutjadhav966@gmail.com>
2025-03-16 22:42:23 +0530

    Add the post function to add data in db

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"log"
 	"context"
+	"log"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type MongoInstance struct {
-	Client	*mongo.Client
-	Db		*mongo.Database
+	Client *mongo.Client
+	Db     *mongo.Database
 }
 
 var mg MongoInstance
@@ -27,9 +26,9 @@ type Employee struct {
 	Name   string  `json:"name"`
 	Salary float64 `json:"salary"`
 	Age    float64 `json:"age"`
-	
-	//mongo responses in bson format and from server we get json format 
-	//so except id everything is same in bson and json 
+
+	//mongo responses in bson format and from server we get json format
+	//so except id everything is same in bson and json
 	//its more like we are telling ID as string in Go is represented as id in json and _id in bson
 	//generally we need to do marshalling and unmarshalling but fiber makes it more easy....
 }
@@ -70,17 +69,19 @@ func main() {
 	}
 	app := fiber.New() //just like starting an app in express
 
+	
+	
 	app.Get("/employee", func(c *fiber.Ctx) error {
-		query := bson.D{{}}//empty query to get all records from database
-		
+		query := bson.D{{}} //empty query to get all records from database
+
 		var employees []Employee = make([]Employee, 0)
-		cursor, err:= mg.Db.Collection("employees").Find(c.Context(),query)
-		if err!=nil{
+		cursor, err := mg.Db.Collection("employees").Find(c.Context(), query)
+		if err != nil {
 			return c.Status(500).SendString(err.Error())
 		}
 		//cursor points to all employees in the database
 
-		if err:= cursor.All(c.Context(),&employees); err!=nil{
+		if err := cursor.All(c.Context(), &employees); err != nil {
 			return c.Status(500).SendString(err.Error())
 		}
 		//here we are converting the content of cursor which is bson format to format go can understand
@@ -88,11 +89,52 @@ func main() {
 		return c.JSON(employees)
 		//again converting it back to JSON so as response to say postman since it can understand only json format
 
-		
 	})
 	//here c is for response and request
 
-	app.Post("/employee")
+
+
+	app.Post("/employee", func(c *fiber.Ctx) error {
+		collection := mg.Db.Collection("employees")
+
+		var employee Employee
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+		//converts the post request from postman into struct format which go understands
+
+		//now we have converted the reqeust into our struct format
+		//we will set the ID value to empty, this is way to tell mongo db that while saving the data
+		//of employee create your own id
+
+		employee.ID = ""
+
+		insertionResult, err := collection.InsertOne(c.Context(), employee)
+		//we saved it in database and saved the result in insertionResult
+
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		//now to confirm if the data is added in database lets return it back
+
+		filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}}
+		//query to get the id
+
+		createdRecord := collection.FindOne(c.Context(), filter)
+		//more like a reverse engineering to get back the record we just saved
+
+		var createdEmployee Employee
+
+		createdRecord.Decode(createdEmployee)
+		//decoding it in struct format the record we got from mongo
+
+		return c.Status(201).JSON(createdEmployee)
+		//returned to frontend back in JSON format
+	})
+
+	
 	app.Put("/employee/:id")
 	app.Delete("/employee/:id")
 }
