6bfe21418282b127ec3610569400d90de15cfdcc
AvadhutJadhav96 <avadhutjadhav966@gmail.com>
2025-03-16 22:16:33 +0530

    Add the get employee function

--- main.go
+++ main.go
@@ -23,10 +23,15 @@ const dbName = "fiber-here"
 const mongoURI = "mongodb://localhost:27017"
 
 type Employee struct {
-	ID     string
-	Name   string
-	Salary float64
-	Age    float64
+	ID     string  `json:"id,omitempty" bson: "_id,omniempty`
+	Name   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    float64 `json:"age"`
+	
+	//mongo responses in bson format and from server we get json format 
+	//so except id everything is same in bson and json 
+	//its more like we are telling ID as string in Go is represented as id in json and _id in bson
+	//generally we need to do marshalling and unmarshalling but fiber makes it more easy....
 }
 
 func Connect() error {
@@ -66,7 +71,24 @@ func main() {
 	app := fiber.New() //just like starting an app in express
 
 	app.Get("/employee", func(c *fiber.Ctx) error {
+		query := bson.D{{}}//empty query to get all records from database
+		
+		var employees []Employee = make([]Employee, 0)
+		cursor, err:= mg.Db.Collection("employees").Find(c.Context(),query)
+		if err!=nil{
+			return c.Status(500).SendString(err.Error())
+		}
+		//cursor points to all employees in the database
 
+		if err:= cursor.All(c.Context(),&employees); err!=nil{
+			return c.Status(500).SendString(err.Error())
+		}
+		//here we are converting the content of cursor which is bson format to format go can understand
+
+		return c.JSON(employees)
+		//again converting it back to JSON so as response to say postman since it can understand only json format
+
+		
 	})
 	//here c is for response and request
 
